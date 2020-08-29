d7d9f6dbeba78d987fd036145db3c6db4157f2c1
Ian Millard <steepleian@gmail.com>
2020-08-29 16:36:45 +0100

    Update windspeeddirection.php

--- www/windspeeddirection.php
+++ www/windspeeddirection.php
@@ -1,6 +1,6 @@
 <?php 
 //original weather34 script original css/svg/php by weather34 2015-2019 clearly marked as original by weather34//
-require_once('w34CombinedData.php');require_once('common.php');?>
+require_once('livedata.php');require_once('common.php');?>
 <meta http-equiv="Content-Type: text/html; charset=UTF-8" />
 <style>.thearrow2{-webkit-transform:rotate(<?php echo $weather["wind_direction"];?>deg);-moz-transform:rotate(<?php echo $weather["wind_direction"];?>deg);-o-transform:rotate(<?php echo $weather["wind_direction"];?>deg);-ms-transform:rotate(<?php echo $weather["wind_direction"];?>deg);transform:rotate(<?php echo $weather["wind_direction"];?>deg);position:absolute;z-index:200;top:0;left:50%;margin-left:-5px;width:10px;height:50%;-webkit-transform-origin:50% 100%;-moz-transform-origin:50% 100%;-o-transform-origin:50% 100%;-ms-transform-origin:50% 100%;transform-origin:50% 100%;-webkit-transition-duration:3s;-moz-transition-duration:3s;-o-transition-duration:3s;-ms-transition-duration:3s;transition-duration:3s}.thearrow2:after{content:'';position:absolute;left:50%;top:0;height:10px;width:10px;background-color:NONE;width:0;height:0;border-style:solid;border-width:14px 9px 0 9px;border-color:RGBA(255,121,58,1) transparent transparent transparent;-webkit-transform:translate(-50%,-50%);-moz-transform:translate(-50%,-50%);-o-transform:translate(-50%,-50%);-ms-transform:translate(-50%,-50%);transform:translate(-50%,-50%);-webkit-transition-duration:3s;-moz-transition-duration:3s;-o-transition-duration:3s;-ms-transition-duration:3s;transition-duration:3s}
 .thearrow2:before{content:'  o o o';color:rgba(255, 124, 57, 1);font-family:Arial, Helvetica, sans-serif;font-size:6px;width:6px;height:6px;position:absolute;z-index:9;left:2px;top:-5px;border:2px solid RGBA(255,255,255,0.8);-webkit-border-radius:100%;-moz-border-radius:100%;-o-border-radius:100%;-ms-border-radius:100%;border-radius:100%}
@@ -10,15 +10,14 @@ require_once('w34CombinedData.php');require_once('common.php');?>
 .avgw{ width:27px; height:27px;	margin-left:35px;-webkit-transform:rotate(<?php echo $weather["wind_direction_avg"];?>deg);-moz-transform:rotate(<?php echo $weather["wind_direction_avg"];?>deg);-o-transform:rotate(<?php echo $weather["wind_direction_avg"];?>deg);-ms-transform:rotate(<?php echo $weather["wind_direction_avg"];?>deg);transform:rotate(<?php echo $weather["wind_direction_avg"];?>deg);}spancalm{postion:relative;font-family:weathertext,Arial;font-size:26px;}</style>
 <div class="updatedtime"><span><?php if(file_exists($livedata)&&time()- filemtime($livedata)>300)echo $offline. '<offline> Offline </offline>';else echo $online." ".$weather["time"];?></div> <br />
 <div class="windspeedvalues"><div class="windspeedvalue">
-<?php
-if ($weather["wind_units"]=='kts'){$weather["wind_units"]="kn";}
+<?php  
 //weather34-windspeed instantaneous
 if ($weather["wind_speed"]<10){echo "&nbsp;".number_format($weather["wind_speed"],1);}else echo number_format($weather["wind_speed"],1);?>
 <div class="windunitidspeed"><?php echo $lang['Currently'];?></div><div class="windunitspeed"><?php echo $weather["wind_units"]?></div></div>
 <div class="windgustvalue">
 <?php 
 //weather34-windgust
-if ($weather["wind_gust_10min"]*$toKnots>=26.9978){echo "<windred>",number_format($weather["wind_gust_10min"],1),"</span>";}else if ($weather["wind_gust_10min"]*$toKnots>=21.5983){echo "<windorange>",number_format($weather["wind_gust_10min"],1),"</span>";}else if ($weather["wind_gust_10min"]*$toKnots>=16.1987){echo "<windgreen>",number_format($weather["wind_gust_10min"],1),"</span>";}else if ($weather["wind_gust_10min"]<10){echo "&nbsp;",number_format($weather["wind_gust_10min"],1);}else echo number_format($weather["wind_gust_10min"],1);?>
+if ($weather["wind_gust_speed"]*$toKnots>=26.9978){echo "<windred>",number_format($weather["wind_gust_speed"],1),"</span>";}else if ($weather["wind_gust_speed"]*$toKnots>=21.5983){echo "<windorange>",number_format($weather["wind_gust_speed"],1),"</span>";}else if ($weather["wind_gust_speed"]*$toKnots>=16.1987){echo "<windgreen>",number_format($weather["wind_gust_speed"],1),"</span>";}else if ($weather["wind_gust_speed"]<10){echo "&nbsp;",number_format($weather["wind_gust_speed"],1);}else echo number_format($weather["wind_gust_speed"],1);?>
 <div class="windunitgust"><?php echo  $weather["wind_units"]?></div>
 <div class="windunitidgust"><?php echo $lang['Gust']; ?></div></span></div></div>
 <div class="windspeedtrend1">
@@ -42,15 +41,15 @@ else if ($weather["wind_units"]=="m/s" && $weather["wind_gust_speed"]*$toKnots>=
 else if ($weather["wind_units"]=="m/s" && $weather["wind_gust_speed"]*$toKnots>=16.1987){echo "<div class=windconvertercircleblue1>".number_format($weather["wind_gust_speed"]*3.60000288,1)." <smallrainunit>kmh</smallrainunit>";}
 else if ($weather["wind_units"]=="m/s" && $weather["wind_gust_speed"]*$toKnots<16.1987){echo "<div class=windconvertercirclegreen1><tgreen>".number_format($weather["wind_gust_speed"]*3.60000288,1)." </tgreen><smallrainunit>kmh</smallrainunit>";}?>
 </div></div>
-<div class="homeweathercompass1"><div class="homeweathercompass-line1"><?php if($weather["wind_direction"]>0){echo '<div class="thearrow2"></div><div class="thearrow1"></div>';}?></div>
-<div class="text1"><div class="windvalue1" id="windvalue"><?php if($weather["wind_direction"]==0) echo $lang['Calm']; else echo $weather["wind_direction"],'&deg;';?></div> </div>
+<div class="homeweathercompass1"><div class="homeweathercompass-line1"><div class="thearrow2"></div><div class="thearrow1"></div></div>
+<div class="text1"><div class="windvalue1" id="windvalue"><?php echo $weather["wind_direction"],'&deg;';?></div> </div>
 <div class="windirectiontext1">
 <?php  //weather34 wind direction value output   
-if($weather["wind_direction"]>0){if($weather["wind_direction"]<=11.25){echo $lang['Northdir'] ;}else if($weather["wind_direction"]<=33.75){echo $lang['NNEdir'];}else if($weather["wind_direction"]<=56.25){echo $lang['NEdir'];}else if($weather["wind_direction"]<=78.75){echo $lang['ENEdir'];}else if($weather["wind_direction"]<=101.25){echo $lang['Eastdir'];}else if($weather["wind_direction"]<=123.75){echo $lang['ESEdir'];}else if($weather["wind_direction"]<=146.25){echo $lang['SEdir'];}else if($weather["wind_direction"]<=168.75){echo $lang['SSEdir'];}else if($weather["wind_direction"]<=191.25){echo $lang['Southdir'];}  else if($weather["wind_direction"]<=213.75){echo $lang['SSWdir'];}else if($weather["wind_direction"]<=236.25){echo $lang['SWdir'];}else if($weather["wind_direction"]<=258.75){echo $lang['WSWdir'];}else if($weather["wind_direction"]<=281.25){echo $lang['Westdir'];}else if($weather["wind_direction"]<=303.75){echo $lang['WNWdir'];}else if($weather["wind_direction"]<=326.25){echo $lang['NWdir'];}else if($weather["wind_direction"]<=348.75){echo $lang['NWNdir'];}else {echo $lang['Northdir'];}}?>
+if($weather["wind_direction"]<=11.25){echo $lang['Northdir'] ;}else if($weather["wind_direction"]<=33.75){echo $lang['NNEdir'];}else if($weather["wind_direction"]<=56.25){echo $lang['NEdir'];}else if($weather["wind_direction"]<=78.75){echo $lang['ENEdir'];}else if($weather["wind_direction"]<=101.25){echo $lang['Eastdir'];}else if($weather["wind_direction"]<=123.75){echo $lang['ESEdir'];}else if($weather["wind_direction"]<=146.25){echo $lang['SEdir'];}else if($weather["wind_direction"]<=168.75){echo $lang['SSEdir'];}else if($weather["wind_direction"]<=191.25){echo $lang['Southdir'];}  else if($weather["wind_direction"]<=213.75){echo $lang['SSWdir'];}else if($weather["wind_direction"]<=236.25){echo $lang['SWdir'];}else if($weather["wind_direction"]<=258.75){echo $lang['WSWdir'];}else if($weather["wind_direction"]<=281.25){echo $lang['Westdir'];}else if($weather["wind_direction"]<=303.75){echo $lang['WNWdir'];}else if($weather["wind_direction"]<=326.25){echo $lang['NWdir'];}else if($weather["wind_direction"]<=348.75){echo $lang['NWNdir'];}else {echo $lang['Northdir'];}?>
  </div></div> 
 <?php echo ' <div class=weather34windrun>'.$windrunicon.' &nbsp;<grey><valuetext1>',number_format($weather["windrun34"],1);?>
 <grey><weather34windrunspan></valuetext>
-<?php if ($weather["wind_units"] == 'mph') echo 'mi'; else if ($weather["wind_units"] == 'm/s') echo ''; else if ($weather["wind_units"] == 'kn') echo 'NM';else echo 'km';?></weather34windrunspan>
+<?php if ($weather["wind_units"] == 'mph') echo 'mi'; else if ($weather["wind_units"] == 'm/s') echo ''; else if ($weather["wind_units"] == 'kts') echo 'kts';else echo 'km';?></weather34windrunspan>
 </div></div><br /><div class=windrun1><?php echo  $lang['Wind Run']?></div>
 <?php ///weather34 beaufort
 if ($weather["wind_speed_bft"] >= 12) {
